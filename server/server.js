d9184f524612d12fec507a4433b839bcd462b373
Gemma Nash <gemma.pumpr@hotmail.co.uk>
2018-09-04 17:21:54 +0100

    Add GET Todos route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,14 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     })
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    })
 })
 
 app.listen(3000, () => {
