5325c16e8d8f51419ed40d8ec9023c7f83415755
Gemma Nash <gemma.pumpr@hotmail.co.uk>
2018-09-07 15:30:51 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -26,6 +26,22 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+    Todo.findById(id).then((todo) => {
+        if(!Todo)
+        {
+            res.status(404).send();
+        }
+         res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    });
 });
 
 app.listen(3000, () => {
