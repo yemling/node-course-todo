f8ae28eb2b36ea409329b58645a0f0cb16f61f15
Gemma Nash <gemma.pumpr@hotmail.co.uk>
2018-09-11 12:18:10 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {
@@ -72,6 +73,34 @@ app.delete('/todos/:id', (req, res) => {
         .catch((e) => {
             res.status(404).send();
         });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']); //lodash pick restricts which elements can be chosen from an object 
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {
+        new: true
+    })
+    .then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(port, () => {
